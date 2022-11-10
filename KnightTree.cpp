d22421beb9b52920f31a81ab531fb581aec808f3
Welson <georgeezaher21@gmail.com>
2022-11-11 00:23:06 +0200

    V1.13

--- KnightTree.cpp
+++ KnightTree.cpp
@@ -8,15 +8,33 @@ KnightTree::KnightTree(string data) {
     size=1;
     level=0;
 }
-void KnightTree::addNodes() {
+void KnightTree::addNodes() {//if level =b create level b+1
     ++level;
     size+=pow(8,level);
-    for(int i=0;i<8;i++){
+    NodePointer ptr=NULL;
+    for(int i=0;i<8;i++){//b3 NULL c2
         char temp[2];
         temp[0]=this->Original->data[0]+this->xMoves[i];
         temp[1]=this->Original->data[1]+this->yMoves[i];
+        if(temp[0]<'a'||temp[0]>'h'||temp[1]<'0'||temp[1]>'8')
+            continue;
         string str(temp);
         this->Original->next[i]=new Node(temp);  //b4 --> d5
+    }// b4 c6 NULL f7 NULL
+    for(int i=0;i<8;i++){
+        bool escape=true;
+        for(int k=i;k<8;k++){
+            if(this->Original->next[k]!=NULL) {
+                escape = false;
+                break;
+            }
+        }
+        if(escape==true)break;
+        while(this->Original->next[i]==NULL){
+            for(int j=i;j<7;j++){
+                this->Original->next[j] = this->Original->next[j + 1];
+            }
+        }
     }
 }
 
